27688bf36ce0fb263d991dd72c83b878d3318dfa
zhengzhixiong <358744287@qq.com>
2015-10-13 20:43:01 +0800

    插件配置

---
+++ src/android/com/jmax/plugins/JmaxAppPlugin.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013-2015 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jmax.plugins;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.widget.Toast;
+
+/**
+ * 
+ * @author max.zheng
+ * @create 2015-10-13下午8:12:55
+ * @since 1.0
+ * @see
+ */
+public class JmaxAppPlugin extends CordovaPlugin {
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+		if (action.equals("toast")) {
+			this.toast(args.getString(0), callbackContext);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @author max.zheng
+	 * @create 2015-10-13下午8:16:04
+	 * @modified by
+	 * @param msg
+	 * @param callbackContext
+	 */
+	private void toast(final String msg,final CallbackContext callbackContext)
+	{
+		 this.cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+            	Toast.makeText(cordova.getActivity(), msg, 1000).show();
+            }
+        });
+	}
+}
---
+++ www/jmaxAppPlugin.js
@@ -0,0 +1,16 @@
+var exec = require('cordova/exec');
+
+function JmaxAppPlugin() {}
+/** 
+ *  exec一共5个参数 
+ *  第一个 :成功回调 
+ *  第二个 :失败回调 
+ *  第三个 :将要调用的类的配置名字(在config.xml中配置 稍后在下面会讲解) 
+ *  第四个 :调用的方法名(一个类里可能有多个方法 靠这个参数区分) 
+ *  第五个 :传递的参数  以json数组
+ */
+JmaxAppPlugin.prototype.toast = function(message) {
+	 exec(null, null, "JmaxAppPlugin", "toast", [message]);
+};
+
+module.exports = new JmaxAppPlugin();
